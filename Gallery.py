1ce5801adf47eabcceb057802c011944598b5c57
seamus-stirling <sstirling@falcon.bentley.edu>
2024-12-03 18:57:40 -0500

    Add files via upload

---
+++ Gallery.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import pydeck as pdk
+import streamlit as st
+
+def homepage():
+    st.set_page_config(layout="wide")
+    st.header("Hello and welcome to my website!")
+    return
+
+
+
+def main ():
+    return
+
+main()
