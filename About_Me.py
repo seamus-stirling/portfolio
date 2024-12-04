4e9b592d822529b93b9c01b9f1a346c72f9ddc8a
seamus-stirling <sstirling@falcon.bentley.edu>
2024-12-03 19:34:00 -0500

    Add files via upload

---
+++ About_Me.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import pydeck as pdk
+import streamlit as st
+
+def bio_page ():
+    st.set_page_config(layout="wide")
+    st.header("About Me")
+    column_1, column_2 = st.column_config
+    with column_1:
+        st.write("this is column 1")
+    with column_2:
+        st.write("this is column 2")
+
+def main ():
+    bio_page()
+    return
+
+main()
--- Gallery.py
+++ Gallery.py
@@ -3,9 +3,13 @@ import pydeck as pdk
 import streamlit as st
 
 def homepage():
+    with st.sidebar:
+        st.sidebar.page_link("About_Me.py")
     st.set_page_config(layout="wide")
     st.header("Welcome to my Portfolio!")
     st.write("Thank you for taking the time to visit my web app. I created this website with the intent of sharing projects that I have completed throughout my time at Bentley University. The goal of sharing this website is to give you a better idea of my in class experiences as well as to practice my coding skills. Please feel free to navigate through the pages which include a personal bio, my favorite programs I have written, and various finance related projects I have completed.")
+
+
     return
 
 
