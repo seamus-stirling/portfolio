dd45dc0548d8962c8bc7723295325c988e86c4a4
seamus-stirling <sstirling@falcon.bentley.edu>
2024-12-03 20:30:32 -0500

    Add files via upload

--- Gallery.py
+++ Gallery.py
@@ -5,18 +5,16 @@ from streamlit import title
 
 
 def homepage():
-    st.set_page_config(page_title="Personal Portfolio", layout="wide")
+    st.set_page_config(page_title="Seamus's Personal Portfolio", layout="wide")
+    st.title("Seamus's Personal Portfolio")
     pages = st.navigation([
         st.Page("Home.py", title="Home"),
         st.Page("About_Me.py", title="About Me"),
         st.Page("coding_projects.py", title="Coding Projects")
     ])
     pages.run()
-    st.header("Welcome to my Portfolio!")
-    st.write("Thank you for taking the time to visit my web app. I created this website with the intent of sharing projects that I have completed throughout my time at Bentley University. The goal of sharing this website is to give you a better idea of my in class experiences as well as to practice my coding skills. Please feel free to navigate through the pages which include a personal bio, my favorite programs I have written, and various finance related projects I have completed.")
 
-
-def main ():
+def main():
     homepage()
 
 main()
