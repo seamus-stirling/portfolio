bd632a16b86b04f4e2510613330b5d9a5624c98f
seamus-stirling <sstirling@falcon.bentley.edu>
2024-12-03 20:25:06 -0500

    Add files via upload

--- Gallery.py
+++ Gallery.py
@@ -7,7 +7,7 @@ from streamlit import title
 def homepage():
     st.set_page_config(page_title="Personal Portfolio", layout="wide")
     pages = st.navigation([
-        st.page("Home.py", title="Home"),
+        st.Page("Home.py", title="Home"),
         st.Page("About_Me.py", title="About Me"),
         st.Page("coding_projects.py", title="Coding Projects")
     ])
